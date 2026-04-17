0605e1b2bb5e852a740ff9f60f097b5ef1036a2f
yanissaouri <yaniss.aouri@laplateforme.io>
2026-04-17 16:16:15 +0200

    fix: fixed structure and code

---
+++ src/main/java/model/Professor.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Professor {
+    private int professorID;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String course;
+
+    public Professor(int professorID, String firstName, String lastName, int age, String course){
+
+        this.professorID = professorID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.course = course;
+    }
+
+    public int getProfessorID() {return professorID;}
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public int getAge() {return age;}
+    public String getCourse() {return course;}
+
+    // a modifier quand on aura l'interface
+    @Override
+    public String toString() {
+        return firstName + " " +
+                lastName +
+                "Age: " + age +
+                " Matiere: " + course;
+    }
+}
---
+++ src/main/java/model/Student.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Student {
+    private int studentID;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double grade;
+    private String course;
+
+    public Student(int studentID, String firstName, String lastName, int age, double grade, String course){
+        
+        this.studentID = studentID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.grade = grade;
+        this.course = course;
+
+    }
+    public int getStudentID() {return studentID;}
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public int getAge() {return age;}
+    public double getGrade() {return grade;}
+    public String getCourse() {return course;}
+
+    public void setGrade(double grade){
+        if (grade >= 0 && grade <=20) {
+            this.grade = grade;
+        }
+    }
+
+    // a modifier quand on aura l'interface
+    @Override
+    public String toString() {
+        return firstName + " " + lastName +
+                "Age: " + age +
+                " Matiere: " + course +
+                " Note: " + grade;
+    }
+}
